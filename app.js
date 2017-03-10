1bfb078249c56de86fc3d9df48a497cc4f849a17
Logan Griswold <logan.a.griswold@gmail.com>
2017-03-10 01:04:01 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var MMMM = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+app.use('/:datetime', function(req, res) {
+    var date = new Date(req.params.datetime);
+    
+    if (date == "Invalid Date") {
+        date = new Date(req.params.datetime * 1000);
+        if (date.getTime() / 1000 != req.params.datetime) {
+            return res.end(date.getTime().toString());
+        }
+    }
+    
+    var unix = date.getTime() / 1000;
+    var natural = MMMM[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+    
+    return res.json({
+        unix: unix,
+        natural: natural
+    });
+});
+app.listen(process.env.PORT || 8080);
