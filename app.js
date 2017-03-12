7a9e696bde923b0278fee45015d2a024c8fe8e0d
Logan Griswold <logan.a.griswold@gmail.com>
2017-03-12 20:40:37 +0000

    added readme, updated doc

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ var app = express();
 var path = require('path');
 var MMMM = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-app.use('/', express.static(path.join(__dirname ,"out", "timestamp-microservice-api", "1.0.0")));
+app.use('/', express.static(path.join(__dirname ,"doc")));
 
+/**
+ * @api {get} /{datetime} getTime
+ * @apiDescription Converts either a UNIX or human-readable Date string into a JSON object representing both
+ * @apiName getTime
+ * @apiGroup TimestampMicroservice
+ * @apiParam {String} datetime Either a UNIX timestamp or a human-readable date string
+ * @apiExample {url} Request-Example:
+ *  https://timestamp-microservice-logan.herokuapp.com/1358035200
+ * @apiExample {url} Request-Example:
+ *  https://timestamp-microservice-logan.herokuapp.com/January 13 2013
+ * @apiSuccess {Number} unix The UNIX timestamp of the input date
+ * @apiSuccess {String} natural The human readable datestring of the input date
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+ *      "unix":1358035200,"natural":"January 13 2013"
+ *  }
+ */
 app.use('/:datetime', function(req, res) {
     var date = new Date(req.params.datetime);
     
